2baaaaf91361dcb1780effe8811f88c59e74703d
Mahmoud Saaed Romeh <romeheng1@gmail.com>
2026-02-27 20:39:17 +0200

    crate Navbar, About and PortfolioSection Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
 import './App.css'
 import ComingSoon from './ComingSoon'
+import About from './Components/About.JSX'
+import Navbar from './Components/Navbar'
+import PortfolioSection from './Components/Portfolio/PortfolioSection'
 
 function App() {
 
   return (
     <>
-      <ComingSoon />
+     <ComingSoon />
+      
+      {/* <Navbar />
+      <About />
+      <PortfolioSection /> */}
     </>
   )
 }
