bd1fba06f6fb6627704873a16caf603d11168fe7
Mahmoud Saaed Romeh <romeheng1@gmail.com>
2026-02-28 14:27:13 +0200

    crate HeroSection, ContactSection and AnimatedSection Components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 import './App.css'
 import ComingSoon from './ComingSoon'
 import About from './Components/About.JSX'
+import AnimatedSection from './Components/AnimatedSection'
+import ContactSection from './Components/ContactSection'
+import HeroSection from './Components/HeroSection'
 import Navbar from './Components/Navbar'
 import PortfolioSection from './Components/Portfolio/PortfolioSection'
 
@@ -9,11 +12,23 @@ function App() {
 
   return (
     <>
-     <ComingSoon />
+     {/* <ComingSoon /> */}
       
-      {/* <Navbar />
-      <About />
-      <PortfolioSection /> */}
+      <Navbar />
+
+      <HeroSection />
+
+<AnimatedSection>
+        <About />
+      </AnimatedSection>
+
+      <AnimatedSection>
+        <PortfolioSection />
+      </AnimatedSection>
+
+      <AnimatedSection>
+        <ContactSection />
+      </AnimatedSection>
     </>
   )
 }
